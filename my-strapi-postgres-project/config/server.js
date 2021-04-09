040e19610ce8b4e7b49ea7d92f854b0d09805b19
Sarkan Aliyev <sarkanaliyev@gmail.com>
2021-04-09 18:27:41 +0400

    first commit

---
+++ my-strapi-postgres-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ef45771582bfe929fa0a24f31ce44c2a'),
+    },
+  },
+});
---
+++ my-strapi-postgres-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4bda7c59-d537-499f-9deb-9f611a1e1ea4'
+};
